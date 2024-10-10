e327593b23befac3f918d7068feabdf6ff02db07
Yulya-Z <zukovayulya@gmail.com>
2024-10-10 23:48:39 +0300

    Add files via upload

---
+++ Module_4_2.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+        inner_function # не работает
+
+    inner_function() # ошибка
+
+test_function() # работает
